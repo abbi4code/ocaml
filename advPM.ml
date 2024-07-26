4bf6520c34ab39291ff4f65f0f63650ade9474e1
abhishek <abhishek4code@gmail.com>
2024-07-26 18:15:41 +0530

    append function and length function created

--- advPM.ml
+++ advPM.ml
@@ -31,3 +31,23 @@ let () = print_endline (string_of_int (sum nums))
 
 
 (* find the length of the list *)
+let names = ["abhishek"; "ram";"shyam"; "nothing" ];;
+
+let rec totalcount lst = 
+  match lst with
+  | [] -> 0
+  | h ::t -> 1 + totalcount t
+;;
+print_endline(string_of_int(totalcount names) ^ " total lsit count");;
+
+(* appending one list with the other *)
+
+let list1 = [12;45;23];;
+let list2 = [56;67;87];;
+
+let rec append lst1 lst2 = 
+  match lst1 with
+  | [] -> lst2
+  | h::t -> h::append t lst2
+
+  
