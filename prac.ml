35282e4cc573ec673e1bace08cad8ed55798fb42
abhishek <abhishek4code@gmail.com>
2024-08-01 01:16:19 +0530

    added

--- prac.ml
+++ prac.ml
@@ -48,8 +48,34 @@ let swap (int1,int2)  =
 (* squaring the element of the lists *)
 
 let sq x = x*x;;
-
 let sqlist = List.map sq [12;23;123;32];;
+
+(* file handlinggg *)
+
+(* readflie
+open 
+input_line *)
+
+let read_file filename =
+  let ic = open_in filename in 
+  try
+    let rec read_lines() =
+    try
+      let line = input_line ic in
+      print_endline(line);
+      read_lines()
+    with
+    | End_of_file -> ();
+  in
+  read_lines();
+  close_in ic
+with
+| e -> 
+       close_in_noerr ic;
+       raise e
+;;
+
+
 
 
 
