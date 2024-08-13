03c7085e71dcd996371c1e83e4c82858f4a0c924
abhishek <abhishek4code@gmail.com>
2024-08-13 17:49:57 +0530

    learning about arrays

--- solution.ml
+++ solution.ml
@@ -1,42 +1,5 @@
-open Str
 
-(* extracting out integers from the each line and putting them into the list and reversing them to get the correct order *)
-let extract_integers line = 
-  let exp = regexp "[0-9]+" in
-  let rec aux acc pos =
-    try
-      let _ = search_forward exp line pos in 
-      let num = matched_string line in 
-      aux (int_of_string num :: acc) (match_end());
-    with 
-        End_of_file -> List.rev acc  
-  in aux [] 0 
-;;
+let list = [34;34;25];;
+let print x = print_endline(string_of_int x );;
 
-(* now taking out each of them and getting the sum and list *)
-
-let process_line line = 
-  let integers = extract_integers line in 
-  let sum = List.fold_left (+) 0 integers in 
-  (integers,sum)
-;;
-
-(* now taking out input from the user depending on input type  *)
-
-let process_input opt_filename =
-  let ic = match opt_filename with
-    | Some filename -> open_in filename
-    | None -> stdin
-in 
-  let rec aux line_sum total_line_sums =
-    let line = input_line ic in
-    let integers,sum = process_line line in 
-    if integers <> [] then
-      Printf.printf "%s=%d\n" 
-      (String.concat " + " (List.map  string_of_int integers)) sum
-    else
-      Printf.printf "=0\n"
-    
-
-
-
+List.iter print list;;
